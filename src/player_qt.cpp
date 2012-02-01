7bc35078dc3c04041e34fad02c357dfff72dfe34
Martin Lambers <marlam@marlam.de>
2012-02-01 10:03:21 +0100

    Tweak zoom: extent the range to 0..4, and avoid overshoot for very narrow screens.

--- src/player_qt.cpp
+++ src/player_qt.cpp
@@ -1437,12 +1437,12 @@ zoom_dialog::zoom_dialog(parameters *params, QWidget *parent) : QDialog(parent),
     QLabel *z_label = new QLabel(_("Zoom:"));
     z_label->setToolTip(_("<p>Set the zoom level for videos that are wider than the screen</p>"));
     _z_slider = new QSlider(Qt::Horizontal);
-    _z_slider->setRange(0, 2000);
+    _z_slider->setRange(0, 4000);
     _z_slider->setValue(params->zoom * 1000.0f);
     _z_slider->setToolTip(z_label->toolTip());
     connect(_z_slider, SIGNAL(valueChanged(int)), this, SLOT(z_slider_changed(int)));
     _z_spinbox = new QDoubleSpinBox();
-    _z_spinbox->setRange(0.0, +2.0);
+    _z_spinbox->setRange(0.0, +4.0);
     _z_spinbox->setValue(params->zoom);
     _z_spinbox->setDecimals(2);
     _z_spinbox->setSingleStep(0.01);
