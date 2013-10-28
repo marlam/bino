bcba7045c99fdb1e496429efd002556216edb0a7
Martin Lambers <marlam@marlam.de>
2013-10-28 21:38:32 +0100

    Fix building with FFmpeg 2.0.x.

--- src/media_object.cpp
+++ src/media_object.cpp
@@ -150,7 +150,8 @@ public:
 // Hide the FFmpeg stuff so that their messy header files cannot cause problems
 // in other source files.
 
-static const size_t audio_tmpbuf_size = (AVCODEC_MAX_AUDIO_FRAME_SIZE * 3) / 2;
+static const size_t max_audio_frame_size = 19200; // 1 second of 48khz 32bit audio
+static const size_t audio_tmpbuf_size = (max_audio_frame_size * 3) / 2;
 
 struct ffmpeg_stuff
 {
