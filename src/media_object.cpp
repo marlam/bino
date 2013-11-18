marlam/bino

commit bcba7045c99fdb1e496429efd002556216edb0a7

Fix building with FFmpeg 2.0.x.

src/media_object.cpp
@@ -150,7 +150,8 @@ public:
 // Hide the FFmpeg stuff so that their messy header files cannot cause problems
 // in other source files.
 
-static const size_t audio_tmpbuf_size = (AVCODEC_MAX_AUDIO_FRAME_SIZE * 3) / 2;
+static const size_t max_audio_frame_size = 19200; // 1 second of 48khz 32bit audio
+static const size_t audio_tmpbuf_size = (max_audio_frame_size * 3) / 2;
 
 struct ffmpeg_stuff
 {

commit 2d3ea1ef0f1ab8c6b739522b43bd05500ec632e8

Fix size of audio buffers for decoding.
This patch fixes bug 40596. The wrong audio buffer size was introduced in
bcba7045c99fdb1e496429efd002556216edb0a7.  Sorry about that.

src/media_object.cpp
@@ -150,7 +150,7 @@ public:
 // Hide the FFmpeg stuff so that their messy header files cannot cause problems
 // in other source files.
 
-static const size_t max_audio_frame_size = 19200; // 1 second of 48khz 32bit audio
+static const size_t max_audio_frame_size = 192000; // 1 second of 48khz 32bit audio
 static const size_t audio_tmpbuf_size = (max_audio_frame_size * 3) / 2;
 
 struct ffmpeg_stuff